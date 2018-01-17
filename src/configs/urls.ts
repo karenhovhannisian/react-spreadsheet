0a8f9011b2329e9e6e7edde4c329bc336d266da6
Hayk Aproyan <hayk.aproyan@iconiq.de>
2018-01-17 21:59:54 +0400

    changed http to https for mockapi urls

--- src/configs/urls.ts
+++ src/configs/urls.ts
@@ -1,15 +1,15 @@
 const urls: any = {
     development: {
         hostname: "localhost",
-        apiUrl: "http://5a57735a751d4e0012779696.mockapi.io/api/v1",
+        apiUrl: "https://5a57735a751d4e0012779696.mockapi.io/api/v1",
     },
     production: {
         hostname: "example.com",
-        apiUrl: "http://5a57735a751d4e0012779696.mockapi.io/api/v1",
+        apiUrl: "https://5a57735a751d4e0012779696.mockapi.io/api/v1",
     },
     test: {
         hostname: "example.com",
-        apiUrl: "http://5a57735a751d4e0012779696.mockapi.io/api/v1",
+        apiUrl: "https://5a57735a751d4e0012779696.mockapi.io/api/v1",
     },
 };
 
